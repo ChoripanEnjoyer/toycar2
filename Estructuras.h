47f61718fa2d828ef80b0dcc3aa00232264043d7
Maxichalla <90658212+Maxichalla@users.noreply.github.com>
2024-05-03 05:46:33 +0000

    version1.1

--- Estructuras.h
+++ Estructuras.h
@@ -13,7 +13,15 @@ struct Accesorio
     int Valor;
 };
 
+struct precios_marcas
+{
+    string Tipo_vehiculo;
+    string Marca_vehiculo;
+    int Precio_marca;
+};
+
 vector<Accesorio> leerAccesoriosDesdeArchivo(const string& nombre_archivo);
+vector<precios_marcas> Leer_precios(const string& nombre_archivo);
 
 void mostrarAccesorios(const vector<Accesorio>& accesorios);
 
